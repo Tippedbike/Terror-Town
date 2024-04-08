53e1a2ced80a74f04925c8c4105b709394864ab7
TippedBike89 <jgalindo-miranda25@aoiths.org>
2024-04-08 12:46:03 -0400

    progress

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerContoller : MonoBehaviour
     private float turnSpeed = 70.0f;  
     private float horizontalInput; 
     private float forwardInput;
+    public float xRange = 15; 
+    public float zRange = 15;
+    public GameObject projectilePrefab; 
     // Start is called before the first frame update
     void Start()
     {
@@ -23,5 +26,30 @@ public class PlayerContoller : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         //rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        if (transform.position.x < -xRange)
+{
+transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+}
+if (transform.position.x > xRange)
+{
+transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+}
+
+if (transform.position.z < -zRange)
+{
+transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+}
+if (transform.position.z > zRange)
+{
+transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+}
+        
+         if ( Input.GetKeyDown(KeyCode.Space))
+        {
+            //launchs projetile from player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
     }
 }
