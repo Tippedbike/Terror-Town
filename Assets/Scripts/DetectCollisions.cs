096b71e9f264a5c56e79717b77fdd277544335e9
Tippedbike <113185655+Tippedbike@users.noreply.github.com>
2024-04-10 21:09:05 -0400

    more progess
    if i cant destroy objects im cooked

---
+++ Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+     if(other.CompareTag("Building"))
+     {
+     Destroy(other.gameObject);
+     Destroy(gameObject);
+     }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
